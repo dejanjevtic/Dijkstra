bf21f8361d2a666f1c5a2161880552a1feaf14e0
dejanjevtic <dejan_d_jevtic@yahoo.com>
2021-04-26 12:00:59 +0200

    Izbaceni komentari

--- src/dijkstra/Dijkstra.java
+++ src/dijkstra/Dijkstra.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dijkstra;
 /* komentar */
 import java.util.*;
 import java.lang.*; 
 import java.io.*; 
-/**
- *
- * @author dejan
- */
+
 public class Dijkstra {
         static final int V = 6; 
         int minDistance(int dist[], Boolean sptSet[]) 
@@ -87,13 +80,11 @@ public class Dijkstra {
                 
             } 
 }
-            // print the constructed distance array 
+            
             printResenje(dist); 
         } 
   
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
         
